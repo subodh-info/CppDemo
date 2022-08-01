940a03a42b7c41dc675294fc1a8b3664006197dc
subodh <subodhkr0692@gmail.com>
2022-08-01 13:22:47 +0530

    just demo for git

--- vectordemo1.cpp
+++ vectordemo1.cpp
@@ -6,5 +6,12 @@ int main(){
     vector<int> vec;
     cout << "size of vector : " << vec.size() << endl;
     cout << "Capacity of vector : " << vec.capacity() << endl;
+
+    vec.push_back(12);
+    vec.push_back(15);
+
+    cout << "new size of vector : " << vec.size() << endl;
+    cout << "new capacity of vector : " << vec.capacity() << endl;
+    
     return EXIT_SUCCESS;
 }
