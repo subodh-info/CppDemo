7e8c8af5f615d8c47d7ec607cb7cb8c9b713232b
subodh <subodhkr0692@gmail.com>
2022-08-01 11:03:39 +0530

    Sum of two integer

--- test.cpp
+++ test.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout << "Welcome to cpp practice" << endl;
+    int a, b;
+    cout << "enter first integer => " << endl;
+    cin >> a;
+    cout << "enter second integ => " << endl;
+    cin >> b;
+
+    int result = a + b;
+
+    cout << "Sum : " << result;
     return EXIT_SUCCESS;
 }
