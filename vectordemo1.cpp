3d32e4bae91281d01d702ce4e584d0392659d7bf
subodh <subodhkr0692@gmail.com>
2022-08-01 11:13:21 +0530

    add new file

---
+++ vectordemo1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> vec;
+    cout << "size of vector : " << vec.size() << endl;
+    cout << "Capacity of vector : " << vec.capacity() << endl;
+    return EXIT_SUCCESS;
+}
