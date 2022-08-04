03d4b9fa80b8fa764e8d2929d5796536c1bede7d
subodh <subodhkr0692@gmail.com>
2022-08-04 15:36:46 +0530

    macros with arguments

---
+++ macros/macros2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#define AREA(l, b) (l * b)
+using namespace std;
+
+int main(){
+    int l = 12;
+    int b = 14;
+    cout << "Aread : " << AREA(l, b) << endl;
+    return 0;
+}
