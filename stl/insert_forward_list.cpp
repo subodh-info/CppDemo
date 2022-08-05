76f907838f888648396415b4b573239a97460d07
subodh <subodhkr0692@gmail.com>
2022-08-05 14:48:44 +0530

    add erase_after() method passed range as argument to it

--- stl/insert_forward_list.cpp
+++ stl/insert_forward_list.cpp
@@ -14,7 +14,6 @@ int main(){
     forward_list<int> flist = {1, 2, 3}; 
     cout << endl << "forward_list => 2 : " ;
     showForwardList(flist);
-
     // insert element from flist.begin();
     cout << endl << "after using insert_after() method ==> " ;
     flist.insert_after(flist.begin(), {10, 20, 30}); 
@@ -29,5 +28,13 @@ int main(){
     // now, show the element of forware_list
     cout << endl << "update forward list ==> " ;
     showForwardList(flist);
+    // erase element by using erase_after() from forwared list at specified position
+    flist.erase_after(ptr);
+    cout << endl << "Now, updated forward list ==> " ;
+    showForwardList(flist); // show elements of forward list
+    // range based erase elements by erase_after() method
+    flist.erase_after(flist.begin(), flist.end());
+    cout << endl << "after erasing element ==> " ;
+    showForwardList(flist); // {1}
     return EXIT_SUCCESS;
 }
