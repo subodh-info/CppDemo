8ff7dc31cdc39cc9828bd93360418f9d8f6ed6e2
subodh <subodhkr0692@gmail.com>
2022-08-08 14:17:52 +0530

    example of erase method with different-different aspect

--- stl/map2.cpp
+++ stl/map2.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 
 void showMap(map<int, string> m){
+    cout << "\n\t Key \t Value\n";
+    cout << "===================================\n";
     map<int, string> :: iterator it;
     for ( it = m.begin(); it != m.end(); ++it ){
-        cout << endl << (*it).first << "=====" << (*it).second;
+        // cout << endl << (*it).first << "=====" << (*it).second;
+        cout << "\t" << (*it).first << "\t" << (*it).second << "\n";
     }
 }
 int main(){
@@ -18,18 +21,26 @@ int main(){
     // another way to insert element into map
     student.insert(pair<int, string>(104, "komal"));
     student.insert(pair<int, string>(105, "amrita"));
+    student.insert(pair<int, string>(106, "Ankush"));
+    student.insert(pair<int, string> (107, "atul"));
+    student.insert(pair<int, string> (108, "ankit"));
 
     // declare and initialize another map
     map<int, string> student2(student.begin(), student.end());
     
     // display list of first student
-    cout << endl << "Roll and name of student => 1";
+    cout << endl << "Roll and name of student => 1\n";
     showMap(student);
     // display list of second student's record
-    cout << endl << "\nRoll and name of student => 2";
+    cout << endl << "\nRoll and name of student => 2\n";
     showMap(student2);
-    cout << endl << "total number of student => " << student.size();
-
-
+    // remove element which key = 103
+    student.erase(103);
+    // now, display student list
+    showMap(student);
+    // remove all element upto element which key less than 105
+    student.erase(student.begin(), student.find(105));
+    // now, display the list of elements
+    showMap(student);
     return 0;
 }
