610d09ec1a5857311f2891adf68e9c4c405783ba
subodh-info <56229881+subodh-info@users.noreply.github.com>
2022-12-26 14:22:05 +0530

    Add files via upload

---
+++ regex/isValidTime.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <regex>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    regex timePattern("([01][0-9]|2[0-3])(:[0-5][0-9]){2}");
+
+    for(int i = 1; i < argc; ++i){
+        string arg(argv[i]);
+        smatch m;
+        
+        if(regex_match(arg, m, timePattern)){
+            cout << arg << " is valid time" << endl;
+        }else{
+            cout << arg << " is not valid time" << endl;
+        }
+    }
+
+    return EXIT_SUCCESS; 
+}
